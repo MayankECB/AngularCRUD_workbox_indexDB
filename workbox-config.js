65ebe16338f2ca33b09c7d5771d969e5bf775d08
Mayank Gupta <mayankgupta.ecb@gmail.com>
2019-06-03 16:19:52 +0530

    Update workbox-config.js
    Add background-sync queue configuration for 'POST', 'PUT' and 'DELETE' operation.

--- workbox-config.js
+++ workbox-config.js
@@ -3,5 +3,45 @@ module.exports = {
   "globPatterns": [
     "**/*.{txt,ico,html,js,css}"
   ],
-  "swDest": "dist\\sw.js"
+  "swDest": "dist\\sw.js",
+  runtimeCaching: [{
+    urlPattern: /http:\/\/localhost:5555\/products/,
+    handler: 'NetworkOnly',
+    method: 'POST',
+    options: {
+      // Configure background sync.
+      backgroundSync: {
+        name: 'product-bgsync-queue',
+        options: {
+          maxRetentionTime: 24 * 60 * 60,
+        },
+      },
+    },
+  }, {
+    urlPattern: /http:\/\/localhost:5555\/products/,
+    handler: 'NetworkOnly',
+    method: 'PUT',
+    options: {
+      // Configure background sync.
+      backgroundSync: {
+        name: 'product-bgsync-queue',
+        options: {
+          maxRetentionTime: 24 * 60 * 60,
+        },
+      },
+    },
+  }, {
+    urlPattern: /http:\/\/localhost:5555\/products/,
+    handler: 'NetworkOnly',
+    method: 'DELETE',
+    options: {
+      // Configure background sync.
+      backgroundSync: {
+        name: 'product-bgsync-queue',
+        options: {
+          maxRetentionTime: 24 * 60 * 60,
+        },
+      },
+    },
+  }]
 };
